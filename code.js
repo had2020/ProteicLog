4fd0fa68beaf71aab4d79d29cdd0bc071d88e73e
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-02-13 15:31:35 -0800

    Update code.js

--- code.js
+++ code.js
@@ -5,3 +5,9 @@ let logged_state = false;
 localStorage.setItem("test1", "hello");
 
 console.log("msg: ", localStorage.getItem("test1"));
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth();
+const day = currentDate.getDate();
+console.log(year, month, day);
