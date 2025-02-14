f6adf88cc43a3df1d623c040ca9ab033a3f7178c
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-02-13 22:28:42 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,15 @@
 console.log("Loaded js...");
 
-let logged_state = false;
-
-localStorage.setItem("test1", "hello");
-
-console.log("msg: ", localStorage.getItem("test1"));
-
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 const month = currentDate.getMonth();
 const day = currentDate.getDate();
 console.log(year, month, day);
+
+function add_meal() {
+  localStorage.setItem("test1", "hello");
+  console.log("msg: ", localStorage.getItem("test1"));
+}
 
 const button2 = document.getElementById("test");
 button2.addEventListener("click", Test);
